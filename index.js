268fcf2ab86a13d6671e0a47011588097a9f0256
Dominic Smith <dominic.smith@loke.com.au>
2017-10-03 18:18:41 +1000

    Expose error

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Breaker } = require('./lib/breaker');
+const { Breaker, BreakerOpenError } = require('./lib/breaker');
 
 exports.Breaker = Breaker;
+exports.BreakerOpenError = BreakerOpenError;
 exports.create = (...args) => new Breaker(...args);
